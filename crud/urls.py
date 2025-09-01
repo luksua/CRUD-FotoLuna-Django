5583d7b24608e8bf9340c6fc356b7c9e4d01cc78
luna <lunasofiaor04@gmail.com>
2025-09-01 13:31:14 -0500

    actualizacion base de proyecto

--- crud/urls.py
+++ crud/urls.py
@@ -20,4 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("todo.urls")),
+    path("", include("citas.urls")),
+    path("", include("eventos.urls")),
+    path("", include("paquetes.urls")),
 ]
